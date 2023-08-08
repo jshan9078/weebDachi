028770c59844213894cd668d8602811d3baa217b
jonnyACCI <jshan9078@gmail.com>
2023-08-08 19:13:02 -0400

    changed landing page

--- weebDachi/src/App.jsx
+++ weebDachi/src/App.jsx
@@ -11,7 +11,7 @@ function App(){
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="/search" element={<SearchPage/>}/>
           <Route path="/view" element={<AnimeView/>}/>
         </Routes>
